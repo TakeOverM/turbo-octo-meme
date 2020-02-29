cb792d51da6ad74c834373392cbd1932db3c4910
bortiz22 <bortiz22@ucmerced.edugit config --global user.email bortiz22@ucmerced.edugit config --global user.email bortiz22@ucmerced.edu>
2020-02-29 10:47:58 -0800

    First commit, changing color and Hello World

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World...!!!!</Text>
+      <Text>Hello World!!!!</Text>
     </View>
   );
 }
@@ -12,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "green",
+    backgroundColor: "brown",
     alignItems: 'center',
     justifyContent: 'center',
   },
